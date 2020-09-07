a3530412217feb1e793b8efd99ea0509f7011122
toilahieu1410 <34303013+toilahieu1410@users.noreply.github.com>
2020-09-07 13:40:14 +0700

    update code 07092020

--- sh/env.sh
+++ sh/env.sh
@@ -4,7 +4,7 @@ export APP_PORT=8002
 
 # config Database env
 export DB_CONNECTION=mongodb
-export DB_HOST=localhost
+export DB_HOST=10.10.15.20
 export DB_PORT=27017
 export DB_NAME=Scada
 export DB_USERNAME=""
