492fe01f7ee648decda81ca399a157ec12cdf895
duongtien512 <duongtien512@gmail.com>
2020-09-12 15:29:36 +0700

    11

--- src/public/js/diagram/diagram.js
+++ src/public/js/diagram/diagram.js
@@ -1,219 +1,124 @@
-
-// Highcharts.chart('chart-diagram1', {
-//     chart: {
-//         type: 'spline',
-//         zoomType: 'x',
-//         backgroundColor:'#0c0b0b40', 
-      
-//         animation: Highcharts.svg, // don't animate in old IE
-//         marginRight: 10,
-//         events: {
-//             load: function () {
-                
-//                 // set up the updating of the chart each second
-//                 var series = this.series[0];
-//                 setInterval(function () {
-//                     $.ajax({
-//                         url: 'http://10.10.11.222:1880/diagram_plc_mitsubishi',
-//                         complete: function(json) {
-//                             data = JSON.parse(json.responseText);
-//                         }
-//                     })
-//                     var x = (new Date()).getTime(), // current time
-//                         y = data.nhietDoKhoiThai
-
-//                     series.addPoint([x, y], true, true);
-//                 }, 5000);
-//             }
-//         }
-//     },
-
-//     time: {
-//         useUTC: false
-//     },
-
-//     // title: {
-//     //     text: 'Live random data'
-//     // },
-
-//     accessibility: {
-//         announceNewData: {
-//             enabled: true,
-//             minAnnounceInterval: 15000,
-//             announcementFormatter: function (allSeries, newSeries, newPoint) {
-//                 if (newPoint) {
-//                     return 'New point added. Value: ' + newPoint.y;
-//                 }
-//                 return false;
-//             }
-//         }
-//     },
-
-//     xAxis: {
-//         type: 'datetime',
-//         tickPixelInterval: 150,
-//         style: {
-//             color: "#fff"
-//          }
-//     },
-
-//     yAxis: {
-//         title: {
-//             text: 'Value',
-//             style: {
-//                 color: "#fff"
-//              }
-//         },
-//         plotLines: [{
-//             value: 0,
-//             width: 1,
-//             color: '#808080'
-//         }]
-//     },
-
-//     tooltip: {
-//         headerFormat: '<b>{series.name}</b><br/>',
-//         pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
-      
-//     },
-
-//     legend: {
-//         enabled: false
-//     },
-
-//     exporting: {
-//         enabled: false
-//     },
-
-//     series: [{
-//         name: 'Random data',
-  
-//         data: (function () {
-//             // generate an array of random data
-//             var data = [],
-//                 time = (new Date()).getTime(),
-//                 i;
-
-//             for (i = -500; i <= 0; i += 1) {
-//                 data.push({
-//                     x: time + i * 1000,
-//                     y: data.nhietDoKhoiThai
-//                 });
-//             }
-//             return data;
-//         }())
-//     }]
-// })
-
+/******************************* JS click move item  *************************************/
 function hightChart (name, dataRequest) {
-    Highcharts.chart(name, {
-        chart: {
-            type: 'spline',
-            zoomType: 'x',
-            backgroundColor:'#0c0b0b40', 
-          
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-                load: function () {
-                    
-                    // set up the updating of the chart each second
-                    var series = this.series[0];
-                    setInterval(function () {
-                        $.ajax({
-                            url: 'http://10.10.11.222:1880/diagram_plc_mitsubishi',
-                            complete: function(json) {
-                                data = JSON.parse(json.responseText);
-                            }
-                        })
-                        var x = (new Date()).getTime(), // current time
-                            y = dataRequest
-    
-                        series.addPoint([x, y], true, true);
-                    }, 5000);
-                }
-            }
-        },
-    
-        time: {
-            useUTC: false
-        },
-    
-        // title: {
-        //     text: 'Live random data'
-        // },
-    
-        accessibility: {
-            announceNewData: {
-                enabled: true,
-                minAnnounceInterval: 15000,
-                announcementFormatter: function (allSeries, newSeries, newPoint) {
-                    if (newPoint) {
-                        return 'New point added. Value: ' + newPoint.y;
+
+            Highcharts.chart(name, {
+            chart: {
+                type: 'spline',
+                zoomType: 'x',
+                backgroundColor:'#0c0b0b40', 
+
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = dataRequest
+                            series.addPoint([x, y], true, true);
+                        }, 5000);
                     }
-                    return false;
                 }
-            }
-        },
-    
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150,
-            style: {
-                color: "#fff"
-             }
-        },
-    
-        yAxis: {
-            title: {
-                text: 'Value',
+            },
+
+            time: {
+                useUTC: false
+            },
+
+            // title: {
+            //     text: 'Live random data'
+            // },
+
+            accessibility: {
+                announceNewData: {
+                    enabled: true,
+                    minAnnounceInterval: 15000,
+                    announcementFormatter: function (allSeries, newSeries, newPoint) {
+                        if (newPoint) {
+                            return 'New point added. Value: ' + newPoint.y;
+                        }
+                        return false;
+                    }
+                }
+            },
+        
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150,
                 style: {
                     color: "#fff"
                  }
             },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-    
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br/>',
-            pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
+        
+            yAxis: {
+                title: {
+                    text: 'Value',
+                    style: {
+                        color: "#fff"
+                     }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+        
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br/>',
+                pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}',
+              
+            },
+        
+            legend: {
+                enabled: false
+            },
+        
+            exporting: {
+                enabled: false
+            },
+        
+            series: [{
+                name: 'Random data',
           
-        },
-    
-        legend: {
-            enabled: false
-        },
-    
-        exporting: {
-            enabled: false
-        },
-    
-        series: [{
-            name: 'Random data',
-      
-            data: (function () {
-                // generate an array of random data
-                var data = [],
-                    time = (new Date()).getTime(),
-                    i;
-    
-                for (i = -500; i <= 0; i += 1) {
-                    data.push({
-                        x: time + i * 1000,
-                        y: dataRequest
-                    });
-                }
-                return data;
-            }())
-        }]
-    })
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+        
+                    for (i = -500; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: dataRequest
+                        });
+                    }
+                    return data;
+                }())
+            }]
+        })
+
 }
 
-hightChart('chart-diagram1', 'nhietDoKhoiThai')
+setInterval(function() {
+    $.ajax({
+        url: 'http://10.10.11.222:1880/diagram_plc_mitsubishi',
+        complete: function(json) {
+            data = JSON.parse(json.responseText);
+
+        },
+        error: function() {
+            console.log('There was an error!');
+        }
+    })
+}, 500)
+hightChart('chart-diagram1', 'data.nhietDoKhoiThai')
+
+// hightChart('chart-diagram2', 'data.nhietDoKhoiThai', 5000, 15000)
+
+
 
 $(document).ready(function(){
     $(".click-btn1").click(function(){
